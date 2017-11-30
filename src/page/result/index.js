a89e5cd2b4b8a6f14fa4b05a99036550833611dc
edwardwang0302 <edwardwang0302@me.com>
2017-11-30 22:07:32 +0800

    Accomplish payment

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -3,7 +3,7 @@
  * @Date:   2017-11-16T22:33:26+08:00
  * @Email:  edwardwang0302@me.com
  * @Last modified by:   moke
- * @Last modified time: 2017-11-19T11:19:36+08:00
+ * @Last modified time: 2017-11-30T22:03:53+08:00
  */
 'use strict';
 require('./index.css');
@@ -13,6 +13,11 @@ var _mm = require('util/mm.js');
 $(function() {
     var type = _mm.getUrlParam('type') || 'default',
         $element = $('.'+type+'-success');
+    if(type === 'payment') {
+        var orderNumber = _mm.getUrlParam('orderNumber'),
+            $orderNumber = $element.find('.order-number');
+        $orderNumber.attr('href', $orderNumber.attr('href') + orderNumber);
+    }
     // 显示对应的提示信息
     $element.show();
 })
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
  * @Date:   2017-11-13T21:15:34+08:00
  * @Email:  edwardwang0302@me.com
  * @Last modified by:   moke
- * @Last modified time: 2017-11-29T20:49:27+08:00
+ * @Last modified time: 2017-11-30T21:05:41+08:00
  */
 var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
@@ -33,6 +33,7 @@ var config = {
         'order-confirm':['./src/page/order-confirm/index.js'],
         'order-list':['./src/page/order-list/index.js'],
         'order-detail':['./src/page/order-detail/index.js'],
+        'payment':['./src/page/payment/index.js'],
         'detail':['./src/page/detail/index.js'],
         'user-login':['./src/page/user-login/index.js'],
         'user-register':['./src/page/user-register/index.js'],
@@ -82,6 +83,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
         new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
         new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
